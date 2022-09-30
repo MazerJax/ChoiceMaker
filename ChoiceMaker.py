37738bf8fd9f662efa22623a60f929b8a918614c
MazerJax98 <64445021+MazerJax98@users.noreply.github.com>
2022-09-30 08:20:54 -0600

    Add files via upload

--- ChoiceMaker.py
+++ ChoiceMaker.py
@@ -9,11 +9,13 @@ def choice_maker():
         input_string = input("Enter a choice:")
         if input_string == "END":
             try:
+                print("Here are your choices:", choice_list)
                 print("Here is your random choice:", choice_list[random.randint(0,len(choice_list))])
             except IndexError:
                 print("You must input atleast 1 choice for the program to run.")
             break
-        elif all(list_value == input_string for list_value in choice_list):
+            
+        elif input_string in choice_list:
             print("This choice has already been inputed. Please input a different choice:")
         else:    
             choice_list.append(input_string)
@@ -22,8 +24,9 @@ def choice_maker():
 
 
 def main():
+
     choice_maker()
-    print(choice_list)
+
 
 
 if __name__ == '__main__':
